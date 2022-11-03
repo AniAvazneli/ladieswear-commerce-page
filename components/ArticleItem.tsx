632ba3b4814698b8db1e826b37c7d99165e3819f
AniAvazneli <avazneli.ani@gmail.com>
2022-11-03 15:26:35 +0400

    add routes and articles

--- components/ArticleItem.tsx
+++ components/ArticleItem.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 
 const ArticleItem = ({article}) => {
   return (
-    <Link href="/article/[id]">
-      
+    <Link legacyBehavior href="/article/[id]" as={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </a>
     </Link>
   );
 };
--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -1,11 +1,12 @@
 import articleStyles from "../styles/Article.module.css";
+import ArticleItem from "./articleItem";
 
 const Article = ({articles}: {articles}) => {
     console.log("listA",articles)
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <h3>{article.title}</h3>
+        <ArticleItem article={article}/>
       ))}
     </div>
   );
